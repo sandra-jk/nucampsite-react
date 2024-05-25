837d2f9c0c0f65b544215a1ba522e787309f2c7f
Sandra <sandrajerotich1@gmail.com>
2024-05-25 18:57:44 +0300

    campsites relocation to right path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
+import CampsiteCard from '.src/features/campsites/CampsiteCard.js';
+import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
   return (
@@ -14,7 +16,7 @@ function App() {
                     </NavbarBrand>
                 </Container>
             </Navbar>
-            I'm ready for workshop!
+            <CampsiteCard campsite={CAMPSITES[0]} />
     </div>
   );
 }

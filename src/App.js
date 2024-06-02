70388ff9589b4027687810d6abbc99b8d29c9b60
Sandra <sandrajerotich1@gmail.com>
2024-06-02 17:49:35 +0300

    campsites slice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import CampsiteCard from '.src/features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesList from './features/campsites/CampsitesList.js';
 
 function App() {
   return (
     <div className="App">
       hello nucamp!
-      <Navbar dark color='primary' sticky='top' expand='md'>
-               <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            <CampsiteCard campsite={CAMPSITES[0]} />
+        <Header />
+        <CampsitesList />
+        <Footer />
     </div>
   );
 }

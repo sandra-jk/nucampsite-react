27872f3607bbd8640f0c37d316b627089c27ea8e
Sandra <sandrajerotich1@gmail.com>
2024-06-09 17:56:37 +0300

    ContactForm Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,12 @@ import { Formik, Field, Form } from "formik"
 
 
 const ContactForm = () => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    };
+
   return (
     <Formik 
     initialValues={{
@@ -14,6 +20,7 @@ const ContactForm = () => {
         contactType: 'By Phone',
         feedback: ''
     }}
+    onSubmit={handleSubmit}
     >
        
         <Form>
@@ -95,7 +102,11 @@ const ContactForm = () => {
                 </Col>
             </FormGroup>
             <FormGroup row>
-
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
+                    </Col>
             </FormGroup>
             
         </Form>

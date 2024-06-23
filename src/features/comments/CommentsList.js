62ae104b4cc26fa713887aba506c5066a5790e8f
Sandra <sandrajerotich1@gmail.com>
2024-06-23 18:02:59 +0300

    Async Comments

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,31 +1,48 @@
-import { Col } from "reactstrap";
-import Comment from "./Comment";
-import { selectCommentsByCampsiteId } from "./commentsSlice";
-import CommentForm from "./CommentForm";
-import { useSelector } from "react-redux";
-
+import { Col } from 'reactstrap';
+import Comment from './Comment';
+import CommentForm from './CommentForm';
+import { selectCommentsByCampsiteId } from './commentsSlice';
+import { useSelector } from 'react-redux';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
 const CommentsList = ({ campsiteId }) => {
-  const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
-  if (comments && comments.length > 0) {
-    return (
-      <Col md="5" className="m-1">
-        <h4>Comments</h4>
-        {comments.map((comment) => {
-          return <Comment key={comment.id} comment={comment} />;
-        })}
-     
-        <CommentForm campsiteId={campsiteId} />
+    const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
-      </Col>
-    );
-  }
-  return(
-    <Col md='5' and className='m-1'>
-        There are no comments for this campsite yet.
-    </Col>
-  );
- 
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
+    const isPosting = useSelector((state) => state.comments.isPosting);
+
+    console.log(`Comments: ${comments}\nisLoading: ${isLoading}\nerrMsg: ${errMsg}`);
+
+    if(isLoading) {
+        return <Loading />
+    };
+
+    if(isPosting) {
+        return <Loading />
+    }
+
+    if(errMsg) {
+        return <Error errMsg={errMsg} />
+    };
+
+    if (comments && comments.length > 0) {
+        return (
+            <Col md='5' className='m-1'>
+                <h4>Comments</h4>
+                {comments.map((comment) => {
+                    return <Comment key={comment.id} comment={comment} />
+                })}
+                <CommentForm campsiteId={campsiteId}></CommentForm>
+            </Col>
+        );
+    }
+    return (
+        <Col md='5' className='m-1'>
+            There are no comments for this campsite yet.
+        </Col>
+    )
 };
 
 export default CommentsList;
